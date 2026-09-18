867ae4160e37b42be282beba16064dcaa422acdf
ak_r <ak_r@git.com>
2026-09-19 00:50:21 +0900

    Initial Commit

---
+++ Amiz/AmizApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmizApp.swift
+//  Amiz
+//
+//  Created by 伊藤彪 on 2026/09/19.
+//
+
+import SwiftUI
+
+@main
+struct AmizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AmizTests/AmizTests.swift
@@ -0,0 +1,19 @@
+//
+//  AmizTests.swift
+//  AmizTests
+//
+//  Created by 伊藤彪 on 2026/09/19.
+//
+
+import Testing
+@testable import Amiz
+
+struct AmizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
